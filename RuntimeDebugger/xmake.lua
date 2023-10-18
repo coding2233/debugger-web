3b6e9a775ceab7db7585054bbb9c4859da1360b5
zhangyang <zhangyang@ghgame.cn>
2023-10-18 16:10:20 +0800

    feat: gk

--- RuntimeDebugger/xmake.lua
+++ RuntimeDebugger/xmake.lua
@@ -1,21 +1,12 @@
 add_requires("libsdl",{alias="sdl"})
--- add_requires("glew")
-add_requires("opengl")
 -- add_requires("libcurl",{alias="curl"})
 -- add_requires("libgit2", {configs = {shared = true}})
 
--- vs开发使用
-add_rules("mode.release","mode.debug")
-
-target("RuntimeDebugger")
-    set_license("MIT")
-    set_kind("binary")
-    set_languages("cxx17")
-    set_arch("x64")
-    add_files("imgui/**.cpp", "src/**.cpp")
-    add_includedirs("imgui","imgui/backends","src")
-    add_packages("sdl","opengl")
-
+-- if is_plat("windows") then
+-- add_requires("glew")
+-- add_requires("opengl")
+-- -- vs开发使用
+-- add_rules("mode.release","mode.debug")
 -- target("RuntimeDebugger_Windows")
 --     set_license("MIT")
 --     set_kind("binary")
@@ -24,6 +15,17 @@ target("RuntimeDebugger")
 --     add_files("imgui/**.cpp", "src/**.cpp")
 --     add_includedirs("imgui","imgui/backends","src")
 --     add_packages("sdl","glew","opengl")
+-- end
+
+
+target("RuntimeDebugger")
+    set_license("MIT")
+    set_kind("binary")
+    set_languages("cxx17")
+    set_arch("x64")
+    add_files("imgui/**.cpp", "src/**.cpp")
+    add_includedirs("imgui","imgui/backends","src")
+    add_packages("sdl")
 
 -- 生成clion可识别的 compile_commands
 -- xmake project -k compile_commands
